7ae106824535007e49a802cc7b88c1090f36ad36
Sadra <sadra.f.001@gmail.com>
2026-04-26 20:14:17 +0330

    fixed ending with activation mistake in FNN.

--- Model/FeedForward.py
+++ Model/FeedForward.py
@@ -4,12 +4,14 @@ from torch import nn
 
 
 class MFeedForward(nn.Module):
-    def __init__(self, inp_dim, outp_dim):
+    def __init__(self, inp_dim):
         super().__init__()
-        self._internal_dim = 2 * inp_dim
+        self._internal_dim = 4 * inp_dim
         self._net = nn.Sequential(
             nn.Linear(inp_dim, self._internal_dim),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.Linear(self._internal_dim, inp_dim), 
+            # Won't End with an activation function so that the new features are not lost + ReLU removes all negative features which hold singal for the model
         )
     def forward(self, x):
         return self._net(x)
