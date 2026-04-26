32f2f3383cabaffc9edaeaf9e6a5fc64425d428c
Sadra <sadra.f.001@gmail.com>
2026-04-26 20:29:31 +0330

    added the code for a tranformer block with self-attention.

---
+++ Model/Transformer.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from .SelfAttention import MSelfAttention
+from .FeedForward import MFeedForward
+
+class MTransformerBlock(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.fnn = MFeedForward(dim, dim)
+        self.norm = nn.LayerNorm(dim)
+        self.atl = MSelfAttention(dim, dim)
+        self.norm2 = nn.LayerNorm(dim)
+
+    def forward(self, x, mask):
+        fnn_out = self.fnn(x) # extract new features
+        x = self.norm(fnn_out + x) # add the input to the output ( Residual connection ) and then normalize
+        attn_out = self.atl(x, mask) # Custom Attention Block/Module
+        x = self.norm2(attn_out + x) # add the input to the output ( Residual connection ) and then normalize
+        return x
