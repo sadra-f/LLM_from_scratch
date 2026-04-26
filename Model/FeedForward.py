a3bb15881a2e9f78e2586be33b3c5ddc9a8f25ed
Sadra <sadra.f.001@gmail.com>
2026-04-26 20:18:44 +0330

    variable rename.

--- Model/FeedForward.py
+++ Model/FeedForward.py
@@ -4,13 +4,13 @@ from torch import nn
 
 
 class MFeedForward(nn.Module):
-    def __init__(self, inp_dim):
+    def __init__(self, dim):
         super().__init__()
-        self._internal_dim = 4 * inp_dim
+        self._internal_dim = 4 * dim
         self._net = nn.Sequential(
-            nn.Linear(inp_dim, self._internal_dim),
+            nn.Linear(dim, self._internal_dim),
             nn.ReLU(),
-            nn.Linear(self._internal_dim, inp_dim), 
+            nn.Linear(self._internal_dim, dim), 
             # Won't End with an activation function so that the new features are not lost + ReLU removes all negative features which hold singal for the model
         )
     def forward(self, x):
