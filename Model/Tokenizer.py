17a440d05c1d5b4dcf84c456d3fa2b9e9e6f0298
Sadra <sadra.f.001@gmail.com>
2026-04-27 16:36:11 +0330

    added decodingto tokenizer.

--- Model/Tokenizer.py
+++ Model/Tokenizer.py
@@ -5,7 +5,9 @@ from transformers import AutoTokenizer
 class MTokenizer():
     def __init__(self):
         self._tokenizer = AutoTokenizer.from_pretrained("gpt2", cache_dir=".cache/")
-
-
+    
     def tokenize(self, data):
         return self._tokenizer(data)
+    
+    def decode(self, data):
+        return self._tokenizer.decode(data, skip_special_tokens=True)
