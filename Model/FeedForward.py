a5102525491230ca89201a6eaec48941f3dd5a97
Sadra <sadra.f.001@gmail.com>
2026-04-27 13:15:40 +0330

    split single dim to in/out dimensions

--- Model/FeedForward.py
+++ Model/FeedForward.py
@@ -4,13 +4,13 @@ from torch import nn
 
 
 class MFeedForward(nn.Module):
-    def __init__(self, dim):
+    def __init__(self, inp_dim, output_dim):
         super().__init__()
-        self._internal_dim = 4 * dim
+        self._internal_dim = 4 * inp_dim
         self._net = nn.Sequential(
-            nn.Linear(dim, self._internal_dim),
+            nn.Linear(inp_dim, self._internal_dim),
             nn.ReLU(),
-            nn.Linear(self._internal_dim, dim), 
+            nn.Linear(self._internal_dim, output_dim), 
             # Won't End with an activation function so that the new features are not lost + ReLU removes all negative features which hold singal for the model
         )
     def forward(self, x):
