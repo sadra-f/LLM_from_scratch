e3fb2c874bb643f686c251c6e5242164d9cb6f76
Sadra <sadra.f.001@gmail.com>
2026-04-30 12:58:18 +0330

    fixed wrongly named attribute.

--- Model/SelfAttention.py
+++ Model/SelfAttention.py
@@ -9,7 +9,7 @@ class MSelfAttention(nn.Module):
         self.Q_W = nn.Linear(inp_dim, self._internal_dim)
         self.K_W = nn.Linear(inp_dim, self._internal_dim)
         self.V_W = nn.Linear(inp_dim, self._internal_dim)
-        self._dim_log = math.sqrt(self._internal_dim)
+        self._dim_sqrt = math.sqrt(self._internal_dim)
         self.dim_adj = nn.Linear(self._internal_dim, outp_dim)
 
     def forward(self, x, mask):
@@ -17,7 +17,7 @@ class MSelfAttention(nn.Module):
         K = self.K_W(x)
         V = self.V_W(x)
         qkt = torch.matmul(Q, torch.transpose(K, -2, -1)) # given (batch, seq, dim) transpose to (batch, dim, seq) for K
-        qkt = qkt / self._dim_log # scale the qkt by the square root to prevent large values that cause softmax to have very small gradients
+        qkt = qkt / self._dim_sqrt # scale the qkt by the square root to prevent large values that cause softmax to have very small gradients
         qkt.masked_fill_(mask == 0, -1e10) # mask is (batch, seq, seq) and we want to fill the positions where mask is 0 with large negative number, also the mask must be applied in a vertical format to the matrix as to mask out unwanted tokens
         weights = torch.softmax(qkt, dim=-1) # gotta provide the dim for softmax not get consfued!
         transformed = torch.matmul(weights, V) # its a matmul not element-wise
