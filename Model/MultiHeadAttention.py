4a59e14f043fc9423cad5287505d44f1f5304790
Sadra <sadra.f.001@gmail.com>
2026-04-30 13:30:56 +0330

    added explicit and implicit multihead attention modules.

--- Model/MultiHeadAttention.py
+++ Model/MultiHeadAttention.py
@@ -1,21 +1,67 @@
 import torch
 from torch import nn
+from .SelfAttention import MSelfAttention
+from .FeedForward import MFeedForward
 
-
-class MultiHeadAttention(nn.Module):
+class EplicitMultiHeadAttention(nn.Module):
     def __init__(self, d_model, num_heads):
         if d_model % num_heads != 0:
             #Decided not to handle this which would lead to a messy code!
             raise ValueError(f"model dimension({d_model}) not divisble by requsted number of attention heads ({num_heads})!")
         super().__init__()
-        self.d_model = d_model
-        self.num_heads = num_heads
+        self._d_model = d_model
+        self._num_heads = num_heads
         self._heads_out_dim = d_model / num_heads
+        self._heads_in_dim = d_model
+        self._proj_layer = MFeedForward(d_model, d_model)
+        self._heads = nn.ModuleList([ MSelfAttention(self._heads_in_dim, self._heads_out_dim) for _ in range(self._num_heads) ])
+        #Concat head outputs
+        self._out_Linear = MFeedForward(d_model, d_model)
+
 
 
 
     def forward(self, x, mask):
-        pass
-        #TODO: Implement with single projetion and later splitting!
+        # _batch, _seq, _model_d = x.shape
+        x = self._proj_layer(x)
+        # att_inps = torch.transpose(x.view(_batch, _seq, self._num_heads, -1), 2, 1) # from (b, t, d) to (b, t, h, d) to (b, h, t, d) ! it was conceptually wrong this way heads won't have access to the full info!
+        tmp_res = []
+        for head in self._heads:
+            tmp_res.append(head(x, mask))
+        x = torch.concat(tmp_res, dim=-1)
+        x = self._out_Linear(x)
+        return x
+    
 
+
+class ImplicitMultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        if d_model % num_heads != 0:
+            #Decided not to handle this which would lead to a messy code!
+            raise ValueError(f"model dimension({d_model}) not divisble by requsted number of attention heads ({num_heads})!")
+        super().__init__()
+        self._d_model = d_model
+        self._num_heads = num_heads
+        self._d_att = d_model / num_heads
+        self._att_scale = torch.sqrt(self._d_att)
+        self._linear_proj = MFeedForward(d_model, d_model)
+        self.q_w = MFeedForward(d_model, d_model)
+        self.k_w = MFeedForward(d_model, d_model)
+        self.v_w = MFeedForward(d_model, d_model)
+        #concat!
+        self._linear_out = MFeedForward()
+
+    def forward(self, x, mask):
+        _batch, _seq, _dim = x.shape
+        x = self._linear_proj(x) # (_batch, _seq, _dim)
+        Q = self.q_w(x).reshape(_batch, _seq, self._num_heads, -1).transpose(2,1) # from (_batch, _seq, _dim) to (_batch, _seq, num_heads, d_att) to (_batch, num_heads, _seq, d_att)
+        K = self.k_w(x).reshape(_batch, _seq, self._num_heads, -1).transpose(2,1) # from (_batch, _seq, _dim) to (_batch, _seq, num_heads, d_att) to (_batch, num_heads, _seq, d_att)
+        V = self.v_w(x).reshape(_batch, _seq, self._num_heads, -1).transpose(2,1) # from (_batch, _seq, _dim) to (_batch, _seq, num_heads, d_att) to (_batch, num_heads, _seq, d_att)
+        QKt = torch.matmul(Q, K.transpose(-2, -1)) / self._att_scale # (_batch, num_heads, _seq, _seq)
+        QKt.masked_fill_(mask == 1, -1e10)
+        att_weights = torch.softmax(QKt, dim=-1)
+        pre_x = torch.matmul(att_weights, V) # (_batch, num_heads, _seq, d_att)
+        x = pre_x.transpose(2,1).reshape(_batch, _seq, -1) # from (_batch, num_heads, _seq, d_att) to (_batch, _seq, num_heads, d_att) to (_batch, _seq, _dim)
+        x = self._linear_out(x)
+        return x
 
