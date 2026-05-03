b815373f1a65104b95f208f5dbe2d11a9da80ecd
Sadra <sadra.f.001@gmail.com>
2026-05-03 12:46:24 +0330

    forced using cached tokenizer.

--- Model/Tokenizer.py
+++ Model/Tokenizer.py
@@ -4,7 +4,9 @@ from transformers import AutoTokenizer
 
 class MTokenizer():
     def __init__(self):
-        self._tokenizer = AutoTokenizer.from_pretrained("gpt2", cache_dir=".cache/")
+        # self._tokenizer = AutoTokenizer.from_pretrained("gpt2", cache_dir=".cache/")
+        #forcing to use the cache! change to your needs.
+        self._tokenizer = AutoTokenizer.from_pretrained(".cache/models--gpt2/snapshots/607a30d783dfa663caf39e06633721c8d4cfcd7e")
     
     def encode(self, data):
         return self._tokenizer(data, add_special_tokens=False)
