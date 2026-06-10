bb469a4283b0b04009c342aadd609721e918d3cb
Sadra <sadra.f.001@gmail.com>
2026-06-10 09:59:03 +0330

    fixed a few minor bugs.

--- Model/MultiHeadAttention.py
+++ Model/MultiHeadAttention.py
@@ -26,15 +26,15 @@ class ExplicitMultiHeadAttention(nn.Module):
         x = self._proj_layer(x)
         # att_inps = torch.transpose(x.view(_batch, _seq, self._num_heads, -1), 2, 1) # from (b, t, d) to (b, t, h, d) to (b, h, t, d) ! it was conceptually wrong this way heads won't have access to the full info!
         tmp_res = []
-        weights = []
+        all_weights = []
         for head in self._heads:
             att_res, weights = head(x, mask)
             tmp_res.append(att_res)
-            weights.append(weights)
+            all_weights.append(weights)
         x = torch.concat(tmp_res, dim=-1)
         x = self._out_Linear(x)
 
-        return x, weights
+        return x, all_weights
     
 
 
@@ -46,8 +46,8 @@ class ImplicitMultiHeadAttention(nn.Module):
         super().__init__()
         self._d_model = d_model
         self._num_heads = num_heads
-        self._d_att = d_model / num_heads
-        self._att_scale = torch.sqrt(torch.tensor(self._d_att))
+        self._d_att = d_model // num_heads
+        self._att_scale = torch.sqrt(torch.tensor(self._d_att)).to(self.device)
         self._linear_proj = nn.Linear(d_model, d_model)
         self.q_w = nn.Linear(d_model, d_model)
         self.k_w = nn.Linear(d_model, d_model)
@@ -65,8 +65,7 @@ class ImplicitMultiHeadAttention(nn.Module):
         QKt = torch.matmul(Q, K.transpose(-2, -1)) / self._att_scale # (_batch, num_heads, _seq, _seq)
         QKt.masked_fill_(mask == 0, -1e10)
         att_weights = torch.softmax(QKt, dim=-1)
-        if self.training:
-            att_weights = self._att_dropout(att_weights)
+        att_weights = self._att_dropout(att_weights) # removed if training since droupout checks it.
         pre_x = torch.matmul(att_weights, V) # (_batch, num_heads, _seq, d_att)
         x = pre_x.transpose(2,1).reshape(_batch, _seq, -1) # from (_batch, num_heads, _seq, d_att) to (_batch, _seq, num_heads, d_att) to (_batch, _seq, _dim)
         x = self._linear_out(x)
